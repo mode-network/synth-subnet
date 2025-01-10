c7ff01fa131228772c9d33ea7836af8f5c1cf97e
Thykof <thykof@protonmail.ch>
2025-01-10 17:41:36 -0500

    check json format and start time

--- simulation/validator/response_validation.py
+++ simulation/validator/response_validation.py
@@ -1,6 +1,11 @@
-def validate_responses(
-    response
-) -> bool:
+import json
+from datetime import datetime, timedelta
+
+
+from simulation.simulation_input import SimulationInput
+
+
+def validate_responses(response, simulation_input: SimulationInput) -> bool:
     """
     Validate responses from miners.
 
@@ -8,5 +13,36 @@ def validate_responses(
     """
     if response is None or len(response) == 0:
         return False
+
+    # assuming `response` is in json
+    response = json.loads(response)
+
+    # check the number of paths
+    if len(response) != simulation_input.num_simulations:
+        return False
+
+    for path in response:
+        # check the number of time points
+        if (
+            len(path)
+            != simulation_input.time_length // simulation_input.time_increment
+        ):
+            return False
+
+        # check the start time
+        if path[0]["time"] != simulation_input.start_time:
+            return False
+
+        for i in range(1, len(path)):
+            # check the time increment
+            i_minus_one_time = datetime.fromisoformat(path[i - 1]["time"])
+            i_time = datetime.fromisoformat(path[i]["time"])
+            expected_delta = timedelta(seconds=simulation_input.time_increment)
+            if i_time - i_minus_one_time != expected_delta:
+                return False
+
+            # check the price format
+            if not isinstance(path[i]["price"], (int, float)):
+                return False
 
     return True
