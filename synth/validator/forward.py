dbc5ba51adfd2c6a38fea9e32c0314b8e8ed1e20
Thykof <thykof@protonmail.ch>
2025-03-11 16:35:11 +0100

    shuffle miners before sending dendrite

--- synth/validator/forward.py
+++ synth/validator/forward.py
@@ -19,6 +19,7 @@
 
 import time
 from datetime import datetime
+import random
 
 import bittensor as bt
 import numpy as np
@@ -303,6 +304,9 @@ async def _query_available_miners_and_save_responses(
         base_neuron.config.neuron.timeout, simulation_input.start_time
     )
 
+    # shuffle the miners to avoid bias (keep miner_uids the same order)
+    miner_uids_shuffled = random.sample(miner_uids, len(miner_uids))
+
     # synapse - is a message that validator sends to miner to get results, i.e. simulation_input in our case
     # Simulation - is our protocol, i.e. input and output message of a miner (application that returns prediction of
     # prices for a chosen asset)
@@ -316,7 +320,9 @@ async def _query_available_miners_and_save_responses(
     # axon is a server application that accepts requests on the miner side
     # ======================================================
     synapses = await base_neuron.dendrite(
-        axons=[base_neuron.metagraph.axons[uid] for uid in miner_uids],
+        axons=[
+            base_neuron.metagraph.axons[uid] for uid in miner_uids_shuffled
+        ],
         synapse=synapse,
         deserialize=False,
         timeout=timeout,
