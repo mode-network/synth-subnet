26d239af9bc27807f08ce01b9997a3cc6981c0cf
Thykof <thykof@protonmail.ch>
2025-06-18 10:48:35 -0500

    improve verify validator permit

--- validator.config.js
+++ validator.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: 'validator',
-      interpreter: 'python3',
-      script: './neurons/validator.py',
-      args: '--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999 --ewma.half_life_days 2.0 --ewma.cutoff_days 4 --softmax.beta -0.003',
-      env: {
-        PYTHONPATH: '.',
-      },
-    },
-  ],
-};
+    apps: [
+        {
+            name: "validator",
+            interpreter: "python3",
+            script: "./neurons/validator.py",
+            args: "--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999 --ewma.half_life_days 2.0 --ewma.cutoff_days 4 --softmax.beta -0.003",
+            env: {
+                PYTHONPATH: ".",
+            },
+        },
+    ],
+}
