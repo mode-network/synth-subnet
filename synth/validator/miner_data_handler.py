3f399f8b25dd8e5768a61098eb0e19b19db1076d
Thykof <thykof@protonmail.ch>
2025-11-28 16:29:03 +0100

    Revert "skip score during cloudflare outage (#189)"
    This reverts commit 42bf3b1906722046321bfe90ec1e4c6599115758.

--- synth/validator/miner_data_handler.py
+++ synth/validator/miner_data_handler.py
@@ -493,7 +493,6 @@ class MinerDataHandler:
                         MinerScore.scored_time,
                         MinerScore.score_details_v3,
                         ValidatorRequest.asset,
-                        ValidatorRequest.start_time,
                     )
                     .select_from(MinerScore)
                     .join(
