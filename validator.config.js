b352a4f13d2caae3bacd4c52358c289d231e36b7
Nathan Seva <thykof@protonmail.ch>
2026-01-22 17:41:17 +0100

    update pm2 config (#216)

--- validator.config.js
+++ validator.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "validator",
+      name: "validator cycle low",
       interpreter: "python3",
       script: "./neurons/validator.py",
-      args: "--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999",
+      args: "--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999 --neuron.nprocs 8 --validator.cycle_name low_frequency",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "validator cycle high",
+      interpreter: "python3",
+      script: "./neurons/validator.py",
+      args: "--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999 --neuron.nprocs 8 --validator.cycle_name high_frequency",
+      env: {
+        PYTHONPATH: ".",
+      },
+    },
+    {
+      name: "validator cycle scoring",
+      interpreter: "python3",
+      script: "./neurons/validator.py",
+      args: "--netuid 50 --logging.debug --wallet.name validator --wallet.hotkey default --neuron.axon_off true --neuron.vpermit_tao_limit 999999 --neuron.nprocs 8 --validator.cycle_name scoring",
       env: {
         PYTHONPATH: ".",
       },
