cf22ad914b12a00fcc45a87035888d2b3fdee42b
Thykof <thykof@protonmail.ch>
2026-04-06 20:48:53 +0200

    hotfix score cursor

--- synth/validator/miner_data_handler.py
+++ synth/validator/miner_data_handler.py
@@ -537,7 +537,7 @@ class MinerDataHandler:
                     },
                 )
 
-            return pd.DataFrame(result.fetchall(), columns=list(result.keys()))
+                return pd.DataFrame(result.fetchall(), columns=list(result.keys()))
         except Exception as e:
             bt.logging.exception(
                 f"in get_miner_scores (got an exception): {e}"
